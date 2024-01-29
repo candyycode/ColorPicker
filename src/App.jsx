6476e7c3791a259f8bb8600997058b51d3ba6b5a
candyycode <perryc@15HPEnvyx360Lap>
2024-01-29 02:23:18 -0600

    added extension

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
 
 // Write your Color component here
-const Color = ({ color, setSelectedColor }) => {
-  return <div onClick={() => setSelectedColor(color)} className={color}></div> 
-}
+const Color = ({ color, selectedColor, setSelectedColor }) => {
+  const isSelected = selectedColor === color;
+
+  return (
+  <div onClick={() => setSelectedColor(color)} 
+  className={`color ${isSelected ? 'selected' : ''}`}
+  style={{ backgroundColor: color }}
+  >
+    {isSelected && <span className="color-name">{color}</span>}
+  </div> 
+  );
+};
+
 
 const App = () => {
   const [selectedColor, setSelectedColor] = useState("");
@@ -14,9 +24,9 @@ const App = () => {
         <div className={selectedColor}>{selectedColor}</div>
       </div>
       <div id="colors-list">
-        <Color color="violet" setSelectedColor={setSelectedColor}/>
-        <Color color="red"  setSelectedColor={setSelectedColor}/>
-        <Color color="blue"  setSelectedColor={setSelectedColor}/>
+        <Color color="violet" selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+        <Color color="red"  selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
+        <Color color="blue" selectedColor={selectedColor} setSelectedColor={setSelectedColor}/>
       </div>
     </div>
   );
